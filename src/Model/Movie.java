2c0a1f5cdd4dc6999f4a637daad79181cfea951e
WTT <wilsonteng89@gmail.com>
2019-11-05 10:43:20 +0800

    update
    added BookingHistory
    Movie class incomplete

---
+++ src/Model/Movie.java
@@ -0,0 +1,16 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Movie {
+	private final String title;
+	private ArrayList<String> directorlist = new ArrayList<String>();
+	private String sypnosis;
+	private ArrayList<String> castlist = new ArrayList<String>();
+	private String showingstatus;
+	private final int agerestriction;
+//	private Review[] reviews;
+	
+	public
+}
