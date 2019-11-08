f1e0dbdcda60b58911039be3cee7553c06e7d4e2
WTT <wilsonteng89@gmail.com>
2019-11-08 17:46:13 +0800

    Update StarterView.java

--- src/View/StarterView.java
+++ src/View/StarterView.java
@@ -1,19 +1,18 @@
 package View;
 
-public class StarterView implements View{
+public class StarterView extends View{
 	public StarterView() {
 		System.out.println("============================\n"
 				+ "|          MOBLIMA         |\n"
 				+ "|   BY CZ2002 SS8 Group5   |\n"
 				+ "============================");
+		displayMenu();
 	} 
 	private void displayMenu() {
 		System.out.println("Menu\n"
 				+ "----\n"
 				+ "(1) Staff\n"
 				+ "(2) MovieGoer\n"
-				+ "(3) Showtime\n"
-				+ "(4) CinemaList\n"
 				+ "Please enter choice");
 	}
 }
