148cd69ab89e8fca52f98037cbaf78c3b17787df
Tyuanyuan <52188590+Tyuanyuan@users.noreply.github.com>
2019-11-09 13:38:46 +0800

    Add files via upload

---
+++ src/View/AdminView.java
@@ -0,0 +1,67 @@
+package View;
+import Presenter.Presenter;
+import java.util.Scanner;
+
+public class AdminView implements View{
+	private boolean loggedIn = false;
+	private String adminName;
+	private String password;
+	public StaffView() {
+		if(loggedIn == false)
+			adminlogin();
+		else
+			displayMenu();
+	}
+	
+	private void adminLogin() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please login to access staff system");
+		System.out.println("Enter Username: ")
+		adminName = sc.next();
+		System.out.println("Enter Password: ")
+		password = sc.next();
+		verifyStaff(adminname,password); //[presenter]
+		if (verifyStaff(adminName, password)) {
+            loggedIn = true;
+            System.out.println("Login successful!");
+            displayMenu();
+        }
+        else {
+            System.out.println("Invalid username or password.");
+            destroy();
+        }
+		
+	}
+	private void displayMenu() {
+		Scanner sc = new Scanner(System.in);	
+		System.out.println("Welcome, please make a selection:\n"
+					+ "(1) Modify movie listing\n"
+					+ "(2) Modify cinema listing\n"
+					+ "(3) Modify movie showtime\n"
+					+ "(4) Configure system settings\n"
+					+ "(5) Logout"
+					+ "Please enter choice");
+			int choice = sc.nextInt();
+			while (passChoiceNumber(choice, 1, 5)) { 
+				switch (choice) {
+	            case 1:
+	                intent(this, new MovieListView());
+	                break;
+	            case 2:
+	                intent(this, new CinemaListView());
+	                break;
+	            case 3:
+	                intent(this, new AdminShowtimeView());
+	                break;
+	            case 4:
+	                intent(this, new SystemSettingView());
+	                break;
+	            case 5:
+	                loggedIn = false;
+	                System.out.println("You have logged out.");
+	                
+	                break;
+				}
+			}
+	}
+}
---
+++ src/View/MovieGoerView.java
@@ -0,0 +1,42 @@
+package View;
+import Presenter.Presenter;
+import java.util.Scanner;
+
+public class MovieGoerView extends View{
+	public MovieGoerView() {
+		displayMenu();
+	}
+	private void displayMenu() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome, please make a selection:"
+				+ "(1) Movie Listing\n"
+				+ "(2) Booking History\n"
+				+ "(3) Movie Review\n"
+				+ "(4) Movie Showtime\n"
+				+ "(5) Book Movie Ticket\n"
+				+ "(6) Return");
+		System.out.println("Enter the number of your choice: ");
+		int choice = sc.nextInt();
+		while (passChoiceNumber(choice, 1, 3)) { 
+			switch (choice) {
+	        case 1:
+	            intent(this, new MovieListingView());
+	            break;
+	        case 2:
+	            intent(this, new BookingHistoryView());
+	            break;
+	        case 3:
+	            intent(this, new ReviewView());
+	            break;
+	        case 4:
+	            intent(this, new CusShowtimeView());
+	            break;
+	        case 5:
+	            intent(this, new BookingView());
+	            break;
+	        case 6:
+	            break;
+			}
+		}
+	}
+}
