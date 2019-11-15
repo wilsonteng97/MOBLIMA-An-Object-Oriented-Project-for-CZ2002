ee9e70064b72f65098778fb3723f28055d6a5d2d
mastermarchewa3454 <47991161+mastermarchewa3454@users.noreply.github.com>
2019-11-15 18:01:01 +0800

    Update CinemaListView.java

--- src/View/admin/CinemaListView.java
+++ src/View/admin/CinemaListView.java
@@ -1,10 +1,21 @@
 package View.admin;
+
 import View.View;
-import static Presenter.CinemaOperatorManager.*;
-import static Presenter.Presenter.*;
+
 import Presenter.Query;
 import Presenter.AdminManager;
+
+import static Presenter.Presenter.*;
+import static Presenter.Query.*;
+import static Presenter.CinemaOperatorManager.*;
+
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import Model.Cinema;
+import Model.CinemaOperator;
+import Model.ShowTime;
+import Model.Enums.CinemaClass;
 
 public class CinemaListView extends View
 {
@@ -21,24 +32,88 @@ public class CinemaListView extends View
 				+ "(3) Return\n");
 		System.out.println("Enter the number of your choice: ");
 		int choice = sc.nextInt();
-		while (verifyChoiceNumber(choice, 1, 3)) { 
+		if (verifyChoiceNumber(choice, 1, 3)) { 
 			switch (choice) {
 		        case 1:
-					displayCinemaList(); //[presenter: displayCinema()] actual method is getCinemaList instead of displayCinema, done by KJ
+					displayCinemaOperator(); //[presenter: displayCinema()] actual method is getCinemaList instead of displayCinema, done by KJ
 		            break;
 		        case 2:
-		        	System.out.println("Enter the cinema you want to add: ");
-		        	String cinemaIn = sc.next();
-		        	addCinema(cinemaIn); 
+		        	newCinema(); 
 		            break;
-		        case 3:
-		        	System.out.println("Enter the cinema you want to remove: ");
-		        	String cinemaIn2 = sc.next();
-		        	removeCinema(cinemaIn2); 
+				case 3:
+					int removecinema = passChoiceInt("Enter the index of the cinema You want to remove ");
+		        	removeCinema(removecinema); 
 		            break;
 		        case 4:
 		            break;
 			}
 		}
+		else 
+		{
+			displayMenu();
+		}
+	}
+
+	private void displayCinemaOperator()
+	{
+		ArrayList<CinemaOperator> cinemaOperatorList;
+		cinemaOperatorList = getCinemaOperators();
+		int numCinemaOperator = 0;
+		for(CinemaOperator cinemaOperator: cinemaOperatorList)
+		{
+			System.out.println(numCinemaOperator + " " + cinemaOperator.getOperatorID() + cinemaOperator.getOperatorName());
+			numCinemaOperator++;
+		}
+
+		int choice = passChoiceInt("Enter which cinema operator's cinema to view");
+		displayCinemaList(cinemaOperatorList.get(choice));
+	}
+
+	private void displayCinemaList(CinemaOperator cinemaOperator)
+	{
+		ArrayList<Cinema> cinemaList = getCinemaList(cinemaOperator);
+		int numCinema =0;
+		for(Cinema cinema: cinemaList)
+		{
+			System.out.println(numCinema + " "+ cinema.getCinemaID() + cinema.getCinemaName());
+		}
+		displayMenu();
+	}
+
+	private void newCinema()
+	{
+		System.out.println("New Cinema");
+		int choice = passChoiceInt("Choice a cinema operator to which add cinema");
+		ArrayList<CinemaOperator> cinemaOperatorsList = getCinemaOperators();
+		CinemaOperator cinemaOperator = cinemaOperatorsList.get(choice);
+
+		String cinemaID = passChoiceString("Enter the cinemaID");
+		String cinemaName = passChoiceString("Enter the cinema name");
+		CinemaClass cinemaClass = null;
+		while(cinemaClass == null)
+		{
+			String cinemaClassIn = passChoiceString("Enter cinema class from the following: \n"
+			+ "NORMAL, PLATINUM");
+			cinemaClass = passCinemaClass(cinemaClassIn);
+		}
+		Cinema cinema = new Cinema(cinemaID, cinemaName, cinemaClass, cinemaOperator);
+		addCinema(cinema);
+
+	}
+
+	private void removeCinema()
+	{
+		System.out.println("Remove Cinema");
+		int choiceOperator = passChoiceInt("Choice a cinema operator from which remove cinema");
+		ArrayList<CinemaOperator> cinemaOperatorsList = getCinemaOperators();
+		CinemaOperator cinemaOperator = cinemaOperatorsList.get(choiceOperator);
+		displayCinemaList(cinemaOperator);
+		ArrayList<Cinema> cinemaList = getCinemaList(cinemaOperator);
+		int choiceCinema = passChoiceInt("Choice a cinema you want to remove");
+		String choice = passChoiceString("Are You sure You want to remove cinema "+ cinemaList.get(choiceCinema).getCinemaName() + "Y/N");
+
+		ShowTime showtime = new Showtime(3, cinema, movie, time);
+		showtime.getCinema();
+
 	}
 }
