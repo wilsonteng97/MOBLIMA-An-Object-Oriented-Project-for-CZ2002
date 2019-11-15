9fbbaa211a8e0c392ae1a8a15711362a50833f2d
mastermarchewa3454 <47991161+mastermarchewa3454@users.noreply.github.com>
2019-11-15 22:20:05 +0800

    Adding Enums

--- src/Model/Enums/ShowingStatus.java
+++ src/Model/Enums/ShowingStatus.java
@@ -1,5 +1,36 @@
 package Model.Enums;
 
 public enum ShowingStatus {
-	COMING_SOON, PREVIEW, NOW_SHOWING, NO_LONGER_AVAILABLE
+	COMING_SOON("COMING_SOON"), PREVIEW("PREVIEW"), NOW_SHOWING("NOW_SHOWING"), NO_LONGER_AVAILABLE("NO_LONGER_AVAILABLE");
+
+	private String string;
+	ShowingStatus(String string)
+	{
+		this.string = string;
+	}
+
+	public static ShowingStatus getValue(String value)
+	{
+		value = value.toUpperCase();
+		if(value.equals("COMING_SOON"))
+		{
+			return COMING_SOON;
+		}
+		else if(value.equals("PREVIEW"))
+		{
+			return PREVIEW;
+		}
+		else if(value.equals("NOW_SHOWING"))
+		{
+			return NOW_SHOWING;
+		}
+		else if(value.equals("NO_LONGER_AVAILABLE"))
+		{
+			return NO_LONGER_AVAILABLE;
+		}
+		else 
+		{
+			return null;
+		}
+	}
 }
