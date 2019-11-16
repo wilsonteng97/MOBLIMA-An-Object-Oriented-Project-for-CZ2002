2be6b3ce16b7397356b10a9bfab23afb6a9e8aa7
Teo-KJ <48685014+Teo-KJ@users.noreply.github.com>
2019-11-16 22:50:07 +0800

    some update

---
+++ src/Data_Initializer/ShowTimeGenerator.java
@@ -0,0 +1,12 @@
+package Data_Initializer;
+
+import java.util.ArrayList;
+import Model.ShowTime;
+
+public class ShowTimeGenerator {
+	public static void Showtimes() {
+		ArrayList<ShowTime> showtimeList = new ArrayList<ShowTime>();
+		
+//		ShowTime(Cinema cinema, Movie movie, Date time)
+	}
+}
