f1bdb9ea27d6c5d36318c7c235a639517c21ee5f
WTT <wilsonteng89@gmail.com>
2019-11-16 22:21:23 +0800

    Update Movie.java

--- src/Model/Movie.java
+++ src/Model/Movie.java
@@ -225,8 +225,8 @@ public class Movie implements Serializable{
 	public static Comparator<Movie> reviewRatingAvgComparator = new Comparator<Movie>() {
 
 		public int compare(Movie m1, Movie m2) {
-			Double average_rating1 = m1.getReviewRatingAverage(m1.getReviewList());
-			Double average_rating2 = m2.getReviewRatingAverage(m2.getReviewList());
+			Double average_rating1 = m1.getReviewRatingAverage();
+			Double average_rating2 = m2.getReviewRatingAverage();
 		   return average_rating2.compareTo(average_rating1);
 		}};
 	
