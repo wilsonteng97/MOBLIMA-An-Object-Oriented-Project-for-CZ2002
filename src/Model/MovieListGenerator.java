87db4f7ff4279505064a936f08d1d0d924f9855a
Teo-KJ <48685014+Teo-KJ@users.noreply.github.com>
2019-11-16 14:15:05 +0800

    merge as required

--- src/Model/MovieListGenerator.java
+++ src/Model/MovieListGenerator.java
@@ -45,12 +45,43 @@ public class MovieListGenerator extends DataManager {
 				"Kristen Stewart, Naomi Scott, Ella Balinska, Elizabeth Banks, Djimon Hounsou, Noah Centineo, Sam Claflin, Patrick Stewart");
 		Movie m3 = new Movie("Charlie's Angels", Model.Enums.AgeRestriction.PG13, Model.Enums.MovieType.MOVIE3D, Model.Enums.ShowingStatus.NOW_SHOWING,
 				"14-11-2019", "Elizabeth Banks", "119 minutes",
-				"Director Elizabeth Banks takes the helm as the next generation of fearless Charlie�s Angels take flight. In Banks� bold vision, Kristen Stewart, Naomi Scott, and Ella Balinska are working for the mysterious Charles Townsend, whose security and investigative agency has expanded internationally. With the world�s smartest, bravest, and most highly trained women all over the globe, there are now teams of Angels guided by multiple Bosleys taking on the toughest jobs everywhere. The screenplay is by Elizabeth Banks from a story by Evan Spiliotopoulos and David Auburn.",
+				"Director Elizabeth Banks takes the helm as the next generation of fearless Charlie Angels take flight. In Banks, Kristen Stewart, Naomi Scott, and Ella Balinska are working for the mysterious Charles Townsend, whose security and investigative agency has expanded internationally. With the world锟絪 smartest, bravest, and most highly trained women all over the globe, there are now teams of Angels guided by multiple Bosleys taking on the toughest jobs everywhere. The screenplay is by Elizabeth Banks from a story by Evan Spiliotopoulos and David Auburn.",
 				false, m3_cast);
 		m3.addTotalSales(1086);
 		list.add(m3);
 		
+
+		ArrayList<String> m4_cast = new ArrayList<String>();
+		m4_cast.add("\r\n" + 
+				"Tom Holland, Samuel L. Jackson, Jake Gyllenhaal, Marisa Tomei, Jon Favreau, Zendaya , Jacob Batalon, Tony Revolori");
+		Movie m4 = new Movie("Spider-Man: Far From Home", Model.Enums.AgeRestriction.PG13, Model.Enums.MovieType.MOVIE3D, Model.Enums.ShowingStatus.NO_LONGER_AVAILABLE,
+				"02-07-2019", "Jon Watts", "129 minutes",
+				"Peter Parker's relaxing European vacation takes an unexpected turn when Nick Fury shows up in his hotel room to recruit him for a mission. The world is in danger as four massive elemental creatures -- each representing Earth, air, water and fire -- emerge from a hole torn in the universe. Parker soon finds himself donning the Spider-Man suit to help Fury and fellow superhero Mysterio stop the evil entities from wreaking havoc across the continent.",
+				false, m4_cast);
+		list.add(m4);
+		
+		ArrayList<String> m5_cast = new ArrayList<String>();
+		m5_cast.add("\r\n" + 
+				"Chloe Bennet, TenzNorgay Trainor, Albert Tsai, Eddie Izzard, Sarah Paulson, Tsai Chin, Michelle Wong");
+		Movie m5 = new Movie("Abominable", Model.Enums.AgeRestriction.PG, Model.Enums.MovieType.DIGITAL, Model.Enums.ShowingStatus.NOW_SHOWING,
+				"07-11-2019", "Jill Culton", "98 minutes",
+				"Abominable” takes audiences on an epic 2,000-mile adventure from the streets of a Chinese city to the breathtaking Himalayan snowscapes. When teenage Yi (Chloe Bennet) encounters a young Yeti on the roof of her apartment building, she and her friends, Jin (Tenzing Norgay Trainor) and Peng (Albert Tsai), name him “Everest” and embark on an epic quest to reunite the magical creature with his family at the highest point on Earth.",
+				false, m5_cast);
+		list.add(m5);
+		
+		ArrayList<String> m6_cast = new ArrayList<String>();
+		m6_cast.add("\r\n" + 
+				"Chloe Bennet, TenzNorgay Trainor, Albert Tsai, Eddie Izzard, Sarah Paulson, Tsai Chin, Michelle Wong");
+		Movie m6 = new Movie("Abominable", Model.Enums.AgeRestriction.PG, Model.Enums.MovieType.DIGITAL, Model.Enums.ShowingStatus.NOW_SHOWING,
+				"07-11-2019", "Jill Culton", "98 minutes",
+				"Abominable” takes audiences on an epic 2,000-mile adventure from the streets of a Chinese city to the breathtaking Himalayan snowscapes. When teenage Yi (Chloe Bennet) encounters a young Yeti on the roof of her apartment building, she and her friends, Jin (Tenzing Norgay Trainor) and Peng (Albert Tsai), name him “Everest” and embark on an epic quest to reunite the magical creature with his family at the highest point on Earth.",
+				false, m6_cast);
+		list.add(m6);
+		
+    String fileName = "datafiles\\movieListFile.txt";
+
     String fileName = "C:\\Users\\tkjie\\Documents\\GitHub\\MOBLIMA-An-Object-Oriented-Project-for-CZ2002\\datafiles\\movieListFile.txt";
+
 		writeDataFile(list, fileName);
 	}
 
--- src/View/AdminView.java
+++ src/View/AdminView.java
@@ -1,25 +1,26 @@
 package View;
-
 import View.admin.MovieListView;
 import View.admin.CinemaListView;
 import View.admin.AdminShowtimeView;
 import View.admin.SystemSettingView;
 
+import static Presenter.AdminManager.*;
 import static Presenter.Presenter.*;
 import static Presenter.LoginManager.*;
+
+import java.io.IOException;
 import java.util.Scanner;
 
-public class AdminView extends View{
+public class AdminView extends View {
 	private boolean loggedIn;
 
 	public AdminView() {
 		loggedIn = true;
 	}
-	
+
 	@Override
-	protected void starter()
-	{
-		if(loggedIn == false)
+	protected void starter() {
+		if (loggedIn == false)
 			adminLogin();
 		else
 			displayMenu();
@@ -32,47 +33,58 @@ public class AdminView extends View{
 		String adminName = sc.next();
 		System.out.println("Enter Password: ");
 		String password = sc.next();
-		if (verifyStaff(adminName, password)) 
-		{
-            loggedIn = true;
-            System.out.println();
-            System.out.println("Login successful!");
-            displayMenu();
-        }
-        else {
-        	System.out.println();
-        	System.out.println("Your username or password could be incorrect.");
-            end();
-        }
-		
+		if (verifyStaff(adminName, password)) {
+			loggedIn = true;
+			System.out.println();
+			System.out.println("Login successful!");
+			displayMenu();
+		} else {
+			System.out.println();
+			System.out.println("Your username or password could be incorrect.");
+			adminLogin();
+		}
+
 	}
+
 	private void displayMenu() {
-		Scanner sc = new Scanner(System.in);		
+		Scanner sc = new Scanner(System.in);
 		System.out.println();
-		System.out.println("Welcome, please make a selection:\n"
-					+ "(1) Modify movie listing\n"
-					+ "(2) Modify cinema listing\n"
-					+ "(3) Modify movie showtime\n"
-					+ "(4) Configure system settings\n"
-					+ "(5) Logout"
-					+ "Please enter your choice:");
-			int choice = sc.nextInt();
-			if (verifyChoiceNumber(choice, 1, 5)) 
-			{ 
-				switch (choice) {
-	            case 1:
-	                intent(this, new MovieListView());
-	                break;
-	            case 2:
-	                intent(this, new CinemaListView());
-	                break;
-	            case 3:
-	                intent(this, new AdminShowtimeView());
-	                break;
-	            case 4:
-	                intent(this, new SystemSettingView());
-	                break;
-	            case 5:
+		System.out.println(
+				"Welcome, please make a selection:\n" + "(1) Modify movie listing\n" + "(2) Modify cinema listing\n"
+						+ "(3) Modify movie showtime\n" + "(4) Configure system settings\n" + "(5) Initialize admins\n"
+						+ "(6) Initialize cinema operator \n" + "(7) Logout\n" + "Please enter your choice:");
+		int choice = sc.nextInt();
+		if (verifyChoiceNumber(choice, 1, 7)) {
+			switch (choice) {
+			case 1:
+				intent(this, new MovieListView());
+				break;
+			case 2:
+				intent(this, new CinemaListView());
+				break;
+			case 3:
+				intent(this, new AdminShowtimeView());
+				break;
+			case 4:
+				intent(this, new SystemSettingView());
+				break;
+			case 5:
+				try {
+					initialiseAdminAccounts();
+				} catch (IOException e1) {
+					System.out.println("Fail to load admin accounts");
+				}
+					displayMenu();
+					break;
+				case 6:
+					try {
+						initialiseCinemaOperators();
+					} catch (IOException e) {
+						System.out.println("Fail to load cinema operators");
+					}
+					displayMenu();
+					break;
+	            case 7:
 	                loggedIn = false;
 	                System.out.println();
 	                System.out.println("You have logged out.");
