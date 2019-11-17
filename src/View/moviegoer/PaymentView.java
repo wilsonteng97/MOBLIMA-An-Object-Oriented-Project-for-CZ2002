963befc8e2ebd2875cec5cb4ef4f50e80aa4c282
mastermarchewa3454 <47991161+mastermarchewa3454@users.noreply.github.com>
2019-11-17 21:20:04 +0800

    Correct payment

--- src/View/moviegoer/PaymentView.java
+++ src/View/moviegoer/PaymentView.java
@@ -1,4 +1,5 @@
 package View.moviegoer;
+
 import static Presenter.BookingManager.*;
 import static Presenter.CinemaOperatorManager.*;
 import View.View;
@@ -7,6 +8,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 import Model.Booking;
 import Model.Customer;
@@ -14,36 +16,39 @@ import Model.Movie;
 import Model.Seat;
 import Model.Transaction;
 import Model.Enums.BookingStatus;
+import Model.Enums.ShowingStatus;
 import Model.Enums.TransactionMethod;
+import static Presenter.Presenter.*;
+import static Model.Enums.TransactionMethod.*;
 
 public class PaymentView extends View{
 	private double price;
     private Seat seat;
     private Customer customer;
-    private String TID;
+    private String id;
     private double GST;
     private double totalPrice;
     	
-    Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
+	TransactionMethod transactionMethod = null;
     public PaymentView(Customer customer, Seat seat, double price) {
     	this.customer = customer;
         this.seat = seat;
         this.price = price;
-    	generateTID();
-    	computeTotalPrice();
+    	setTransactionID();
 	}	
     
     private void displayMenu() throws IOException {
-    	System.out.println("Movie Payment");
-    	System.out.println();
-    	System.out.println("Transaction ID: " + TID);
+		
+    	System.out.println("Movie Payment\n");
+		System.out.println("Transaction ID: " + id);
     	System.out.println("Ticket price: " + price);
     	System.out.println("GST: " + GST);
     	System.out.println("Grand total: " + totalPrice);
         if (customer.getIsSenior()) {
         	System.out.println("50% off for senior citizen");
         }
-
+		setTransactionMethod();
         System.out.println();
         System.out.println("1. Confirm payment");
         System.out.println("2. Cancel");
@@ -57,43 +62,57 @@ public class PaymentView extends View{
             	end();
                 break;
         }
-    }
+	}
+	
+	private void setTransactionMethod()
+	{
+		while(transactionMethod == null)
+		{
+			String choice = passChoiceString("Set the transaction from the following: \n"
+			+"DEBIT_CREDIT, E_WALLET, QR_CODE");
+			transactionMethod = getValueTransactionMethod(choice);
+		}
+	}
    
-    private String generateTID() {
-    	TID = seat.getShowtime().getCinema().getCinemaID() +
-                new SimpleDateFormat("YYYYMMddhhmm").format(new Date().getTime());
-    	return TID;
+    private String setTransactionID() {
+		StringBuilder sb = new StringBuilder();
+		double a = (Math.random()*((9999-1000)+1))+1000;
+		Random r = new Random();
+		for(int i =0; i<4; i++)
+		{
+			char c = (char)(r.nextInt(26) + 'a');
+			sb.append(c);
+		}
+		sb.append(a);
+		id = sb.toString();
+		return id;
     }
     
-    private Double computeTotalPrice() {
-        if (customer.getIsSenior()) 
-        	price /= 2;
-        GST = Math.round(price * 7)/100;
-        totalPrice = Math.round((price+GST)*100)/100;
-        return totalPrice;
-    }
+    // private Double computeTotalPrice() {
+    //     if (customer.getIsSenior()) 
+    //     	price /= 2;
+    //     GST = Math.round(price * 7)/100;
+    //     totalPrice = Math.round((price+GST)*100)/100;
+    //     return totalPrice;
+    // }
     
     private void saveBooking() throws IOException {
         seat.setOccupied(true);
-        Movie movie = seat.getShowtime().getMovie();
-        getMovieList().get(getMovieList().indexOf(movie)).addTotalSales(1);
+		Movie movie = seat.getShowtime().getMovie();
+		movie.addTotalSales(1);
 		Date today = Calendar.getInstance().getTime();
-		Transaction transaction = new Transaction(generateTID(), computeTotalPrice(), 
-												  today, TransactionMethod.DEBIT_CREDIT);
-		
-        Booking record = new Booking(customer, today, transaction, 
-        							 BookingStatus.ACCEPTED, seat);
+		Transaction transaction = new Transaction(setTransactionID(), price, today, transactionMethod);
+        Booking record = new Booking(customer, today, transaction, BookingStatus.ACCEPTED, seat);
         addBooking(record);
         try {
 			updateShowTime();
 	        updateMovieListing();
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Fail to save the booking");
 		}
         System.out.println();
         System.out.println("Payment succeed!");
         intent(this, new MovieListingView());
-        end();
     }
 
 	@Override
